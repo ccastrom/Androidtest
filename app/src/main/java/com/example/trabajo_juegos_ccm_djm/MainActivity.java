88379279027307b77a7dfc2659a519ce507fc9e1
BRAVOSIX <claudio_jr@live.cl>
2021-10-18 15:37:24 -0300

    First commit

---
+++ app/src/main/java/com/example/trabajo_juegos_ccm_djm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.trabajo_juegos_ccm_djm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
